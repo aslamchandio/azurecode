0aceee63320a81e2cdbbea41ebfb6cfab7c20ba1
aslamchandio <32462821+aslamchandio@users.noreply.github.com>
2021-03-08 03:08:12 +0500

    Update install_web.sh

--- install_web.sh
+++ install_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo chown -R azureuser.  /var/www/html
 echo "<html><body><h1>Hello AWS Cloud, This is   WebServer 1</h1></body></html>" > /var/www/html/index.html
-
